63b477222832d2bea9b211fdc71fcd8b11f2d1ce
Dhananjay Sah <dhanjusah123@gmail.com>
2026-01-16 08:42:44 +0545

    string practice

--- string_test.py
+++ string_test.py
@@ -1,7 +1,31 @@
-var1 = 'Hello World !'
-var2 = "Python Programming"
+# var1 = 'Hello World !'
+# var2 = "Python Programming"
 
-# print("var1[0]:", var1[0])
-# print("var2[1:5]", var2[1:5])
-print("var2[::-1]", var2[::-1]) 
-# print("var2[0:8:-1]", var2[0:10:3])
+# # print("var1[0]:", var1[0])
+# # print("var2[1:5]", var2[1:5])
+# print("var2[::-1]", var2[::-1]) 
+# # print("var2[0:8:-1]", var2[0:10:3])
+
+
+# ram='Hello world!'
+# # print("updated string:-", ram[:6] + 'python')
+# print(ram[5]) #prints blank space
+
+# newString = "Hello"*3
+# print(newString)
+
+# sets le duplicate charcters print gardaina, 
+
+# lists in python
+# list1 = ['python', 'programming', 1997, 2000]
+# list2 = [1, 2, 3, 4, 5, 6, 7]
+# print("list1[0]:", list1[0])
+# print("list2[1:5]:", list2[1:5])
+# print("list2[:-1]:", list2[::-1])
+
+list1 = ['python', 'programming', 1997, 2000]
+print(list1)
+del list1[2]
+print("after deleting value at index 2: ", list1)
+
+
