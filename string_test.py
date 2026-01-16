ed64df5e572ecc1dbb758a0fa9d498fbb7a3f4ca
Dhananjay Sah <dhanjusah123@gmail.com>
2026-01-16 08:10:26 +0545

    string indexing and slicing

--- string_test.py
+++ string_test.py
@@ -1,5 +1,7 @@
 var1 = 'Hello World !'
 var2 = "Python Programming"
 
-print("var1[0]:", var1[0])
-print("var2[1:5]", var2[1:5])
+# print("var1[0]:", var1[0])
+# print("var2[1:5]", var2[1:5])
+print("var2[::-1]", var2[::-1]) 
+# print("var2[0:8:-1]", var2[0:10:3])
